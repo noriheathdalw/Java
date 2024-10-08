541ba5496f21eb596ae7f7211c856497fd265ab4
Norika Heathdale <bezantvibrant@gmail.com>
2024-10-08 15:47:18 +1000

    Create Money Spent.java

---
+++ Baddie/Money Spent.java
@@ -0,0 +1,12 @@
+class Baddie {
+  public static void main(String[] args) {
+
+    double moneySpent;
+    moneySpent = 350.75;
+
+    System.out.print("I just spent ");
+    System.out.print(moneySpent);
+    System.out.println(" dollars on my shopping spree.");
+    System.out.println("Treat yourself, always.");
+  }
+}
