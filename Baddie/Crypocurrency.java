c67baaf0d99c345273a61f5d2363f934d367c195
Norika Heathdale <bezantvibrant@gmail.com>
2024-10-08 15:48:16 +1000

    Create Crypocurrency.java

---
+++ Baddie/Crypocurrency.java
@@ -0,0 +1,12 @@
+class Baddie {
+  public static void main(String[] args) {
+
+    double cryptoWallet;
+    cryptoWallet = 0.742;
+
+    System.out.print("I have ");
+    System.out.print(cryptoWallet);
+    System.out.println(" Bitcoin in my crypto wallet.");
+    System.out.println("Just sitting on a fortune.");
+  }
+}
