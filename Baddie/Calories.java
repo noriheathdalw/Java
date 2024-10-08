b16a4015f493df85eff41201bf171ff0f4612665
Norika Heathdale <bezantvibrant@gmail.com>
2024-10-08 15:44:05 +1000

    Create Calories.java

---
+++ Baddie/Calories.java
@@ -0,0 +1,12 @@
+class Baddie {
+  public static void main(String[] args) {
+
+    double caloriesBurned;
+    caloriesBurned = 450.5;
+
+    System.out.print("I just burned ");
+    System.out.print(caloriesBurned);
+    System.out.println(" calories in my workout!");
+    System.out.println("I can do anything.");
+  }
+}
